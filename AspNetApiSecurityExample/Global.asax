b6ac2a98684211d8a7751953c7e28f45760a0a40
xivsolutions <xivsolutions@gmail.com>
2014-01-26 08:10:30 -0600

    Project and directory renaming only - no code changes

---
+++ AspNetApiSecurityExample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspNetApiSecurityExample.MvcApplication" Language="C#" %>
---
+++ AspNetApiSecurityExample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspNetApiSecurityExample.Startup))]
+namespace AspNetApiSecurityExample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
